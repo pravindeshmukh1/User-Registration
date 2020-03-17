b8dd170514c5fe6c44774a643fee736a016da256
pravindeshmukh1 <pravin2391@gmail.com>
2020-03-17 13:04:09 +0530

    ADDED: Mobile Number Validate

--- Userregistration.sh
+++ Userregistration.sh
@@ -3,14 +3,15 @@ shopt -s extglob
 
 echo "Welcome to the User Registration "
 
-echo "Enter the full name "
-read firstName lastName email
+echo "Enter the First, Last Name & Email id & Mobile no :- "
+read firstName lastName email mobileNumber
 
 firstNameRegex="^[A-Z][A-Za-z]{2,}$"
 lastNameRegex="^[A-Z][A-Za-z]{2,}$"
-emailRegex="^[a-z][.a-z]?+@[a-z.]+[a-z][.a-z]?$"
+emailRegex="^[a-z][.a-z]?+@[a-z.]+[a-z]{2,4}[.a-z]{2,4}?$"
+mobileNumberRegex="^[0-9]{2}\s[0-9]{10}$"
 
-function checkUserName() {
+function checkUserDetails() {
 
 	if [[ $1 =~ $2 ]]
 	then
@@ -19,6 +20,17 @@ function checkUserName() {
 			echo "Invalid $1"
 	fi
 }
-checkUserName $firstName $firstNameRegex
-checkUserName $lastName $lastNameRegex
-checkUserName $email	$emailRegex
+checkUserDetails $firstName $firstNameRegex
+checkUserDetails $lastName $lastNameRegex
+checkUserDetails $email	$emailRegex
+
+function checkMobileNumber() {
+
+	if [[ $mobileNumber =~ $mobileNumberRegex ]]
+	then
+			echo "Valid $1"
+	else
+			echo "Invalid $1"
+	fi
+}
+checkMobileNumber
