9e2c51db82a8d6ab336d25fceb6f4e7928162511
pravindeshmukh1 <pravin2391@gmail.com>
2020-03-17 11:26:25 +0530

    ADDED: Check First Name

--- Userregistration.sh
+++ Userregistration.sh
@@ -1,3 +1,20 @@
 #!/bin/bash -x
+shopt -s extglob
 
 echo "Welcome to the User Registration "
+
+
+read -p "Enter the First Name :- " firstName
+
+firstNameRegex="^[A-Z][A-Za-z]{3,}$"
+
+function checkFirstName() {
+
+	if [[ $firstName =~ $firstNameRegex ]]
+	then
+			echo "Valid"
+	else
+			echo "Invalid"
+	fi
+}
+checkFirstName
