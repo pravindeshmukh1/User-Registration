1e000ec70e672c6351038f14ac1f2b0b9ee4bdaf
pravindeshmukh1 <pravin2391@gmail.com>
2020-03-18 12:05:37 +0530

    ADDED: Password Pre-defined Rule1

--- Userregistration.sh
+++ Userregistration.sh
@@ -3,13 +3,21 @@ shopt -s extglob
 
 echo "Welcome to the User Registration "
 
-echo "Enter the First, Last Name & Email id & Mobile no :- "
-read firstName lastName email mobileNumber
+echo "-----Enter the User Details----- "
+#read -p "Enter the First Name:-" firstName
+#read -p "Enter thr Last Name:- " lastName
+#read -p "Enter the Email Id:- " email
+#read -p "Enter the Mobile Numebr:- " mobileNumber
+read -p "Enter the Password :- " password
+
+
 
 firstNameRegex="^[A-Z][A-Za-z]{2,}$"
 lastNameRegex="^[A-Z][A-Za-z]{2,}$"
 emailRegex="^[a-z][.a-z]?+@[a-z.]+[a-z]{2,4}[.a-z]{2,4}?$"
 mobileNumberRegex="^[0-9]{2}\s[0-9]{10}$"
+passwordRegex="[A-Za-z0-9]{8,}"
+
 
 function checkUserDetails() {
 
@@ -34,3 +42,16 @@ function checkMobileNumber() {
 	fi
 }
 checkMobileNumber
+
+
+function passwordValidation() {
+
+if [[ $password =~ $passwordRegex ]]
+        then
+                        echo "Valid"
+        else
+                        echo "Invalid"
+        fi
+
+}
+passwordValidation
