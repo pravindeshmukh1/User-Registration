7db9d198aad77d0f306c35927a9ea78cdbcfb2f3
pravindeshmukh1 <pravin2391@gmail.com>
2020-03-29 19:14:35 +0530

    REFACTOR: Remove the Empty Line

--- Userregistration.sh
+++ Userregistration.sh
@@ -2,15 +2,12 @@
 shopt -s extglob
 
 echo "Welcome to the User Registration "
-
 echo "-----Enter the User Details----- "
-#read -p "Enter the First Name:-" firstName
-#read -p "Enter thr Last Name:- " lastName
-#read -p "Enter the Email Id:- " email
-#read -p "Enter the Mobile Numebr:- " mobileNumber
+read -p "Enter the First Name:-" firstName
+read -p "Enter thr Last Name:- " lastName
+read -p "Enter the Email Id:- " email
+read -p "Enter the Mobile Numebr:- " mobileNumber
 read -p "Enter the Password :- " password
-
-
 
 firstNameRegex="^[A-Z][A-Za-z]{2,}$"
 lastNameRegex="^[A-Z][A-Za-z]{2,}$"
@@ -18,19 +15,16 @@ emailRegex="^[a-z][.a-z]?+@[a-z.]+[a-z]{2,4}[.a-z]{2,4}?$"
 mobileNumberRegex="^[0-9]{2}\s[0-9]{10}$"
 passwordRegex="[A-Za-z0-9]{8,}"
 
-
 function checkUserDetails() {
-
-	if [[ $1 =~ $2 ]]
+	local symbol=$1
+	local pattern=$2
+	if [[ $symbol =~ $pattern ]]
 	then
-			echo "Valid $1"
+			echo "Valid $symbol"
 	else
-			echo "Invalid $1"
+			echo "Invalid $symbol"
 	fi
 }
-checkUserDetails $firstName $firstNameRegex
-checkUserDetails $lastName $lastNameRegex
-checkUserDetails $email	$emailRegex
 
 function checkMobileNumber() {
 
@@ -41,8 +35,6 @@ function checkMobileNumber() {
 			echo "Invalid"
 	fi
 }
-checkMobileNumber
-
 
 function passwordValidation() {
 
@@ -54,4 +46,8 @@ function passwordValidation() {
         fi
 
 }
+checkUserDetails $firstName $firstNameRegex
+checkUserDetails $lastName $lastNameRegex
+checkUserDetails $email	$emailRegex
+checkMobileNumber
 passwordValidation
